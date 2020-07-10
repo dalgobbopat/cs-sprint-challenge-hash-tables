e5076b563fd1256f3e3ad10cc133d1edeb41cff0
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-07-10 13:57:33 -0400

    working on ex4 stuck on making the test work

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # Create a hashtable to store the numbers 
+    hashtable = {}
+    # Put the results in an array to to be able to see which are positive and negative
+    result = []
+    # If number has a negative in hashtable print results 
+    for i in a:
+        if i is -i in hashtable:
+            print(i)
 
     return result
 
